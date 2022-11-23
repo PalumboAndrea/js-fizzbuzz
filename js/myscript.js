aee76aefa991202d9b3536dd255baf394ae4a567
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-23 17:16:22 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -12,22 +12,35 @@ Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qual
 Consigli del giorno:
 Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+BONUS 1:
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+BONUS 2:
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
  * 
  */
 
 for (let i=1; i<=100; i++){
-    
 
+    let el = document.createElement("div");
+    let container = document.getElementById('main-section');
+    container.append(el);
+    
     if ((i % 3 === 0) && (i % 5 === 0)){
-        console.log('FizzBuzz')
+        el.innerHTML += "FizzBuzz";
+        el.style.color = 'red';
     } else if (i % 5 === 0){
-        console.log('Buzz')
+        el.innerHTML += 'Buzz';
+        el.style.color = 'blue';
     } else if (i % 3 === 0){
-        console.log('Fizz')
+        el.innerHTML += 'Fizz';
+        el.style.color = 'green';
     } else {
-        console.log(i)
+        el.innerHTML += i;
+        el.style.color = 'orange';
     }
 
-
-
 }
+
+
+
